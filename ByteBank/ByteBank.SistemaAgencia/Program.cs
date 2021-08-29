335a43221e84058771eee41b5272b0adad8bbc66
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-29 19:09:52 -0300

    Conhecendo Array

---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ARRAY de inteiros, com 5 posições!
+            int[] idades = new int[5];
+
+           
+            Console.ReadLine();
+        }
+    }
+}
